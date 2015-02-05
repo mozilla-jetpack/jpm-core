dcef2fb3425ae728fd548fc29aa6c0bf74474a4b
Erik Vold <evold@mozilla.com>
2015-02-05 11:29:21 -0800

    Using String concatenation which is less likely to regress

--- data/bootstrap.js
+++ data/bootstrap.js
@@ -6,6 +6,6 @@
 const { utils: Cu } = Components;
 const rootURI = __SCRIPT_URI_SPEC__.replace("bootstrap.js", "");
 const COMMONJS_URI = "resource://gre/modules/commonjs";
-const { require } = Cu.import(`${COMMONJS_URI}/toolkit/require.js`, {});
-const { Bootstrap } = require(`${COMMONJS_URI}/sdk/addon/bootstrap.js`);
+const { require } = Cu.import(COMMONJS_URI + "/toolkit/require.js", {});
+const { Bootstrap } = require(COMMONJS_URI + "/sdk/addon/bootstrap.js");
 const { startup, shutdown, install, uninstall } = new Bootstrap(rootURI);
